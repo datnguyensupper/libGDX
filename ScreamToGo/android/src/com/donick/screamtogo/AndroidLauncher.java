199aa36145d927ce8e4cf58ede3dfed0918d1c12
Nguyen Tien Dat <tuynumemories2910@gmail.com>
2017-03-03 01:33:11 +0700

    create scream to go project

---
+++ ScreamToGo/android/src/com/donick/screamtogo/AndroidLauncher.java
@@ -0,0 +1,17 @@
+package com.donick.screamtogo;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.donick.screamtogo.ScreamToGo;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+
+		initialize(new ScreamToGo(), config);
+	}
+}

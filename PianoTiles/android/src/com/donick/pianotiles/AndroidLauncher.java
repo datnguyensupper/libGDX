f5c68d9a1e97de2b96a9c93f7c319c8ea7455803
Nguyen Tien Dat <tuynumemories2910@gmail.com>
2017-03-14 07:31:36 +0700

    add piano tiles project

---
+++ PianoTiles/android/src/com/donick/pianotiles/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.donick.pianotiles;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.donick.pianotiles.PianoTiles;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new PianoTiles(), config);
+	}
+}

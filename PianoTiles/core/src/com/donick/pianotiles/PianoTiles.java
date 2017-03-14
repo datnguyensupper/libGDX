020a7475ba45988357f288bc5d700920b6a945db
Nguyen Tien Dat <tuynumemories2910@gmail.com>
2017-03-15 00:12:36 +0700

    PIano Tiles: add first code to project

--- PianoTiles/core/src/com/donick/pianotiles/PianoTiles.java
+++ PianoTiles/core/src/com/donick/pianotiles/PianoTiles.java
@@ -3,31 +3,57 @@ package com.donick.pianotiles;
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.utils.Array;
 
 public class PianoTiles extends ApplicationAdapter {
 	SpriteBatch batch;
-	Texture img;
+	Texture img,imgTile;
+
+	float gameWidth;
+	float gameHeight;
+	float deviceWidth;
+	float deviceHeight;
+	float scaleRatio;
+	OrthographicCamera camera;
+
+	private Array<Float> arrayOfTiles;
 	
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
 		img = new Texture("badlogic.jpg");
+		imgTile = new Texture("tile.jpg");
+
+		deviceWidth = Gdx.graphics.getWidth();
+		deviceHeight = Gdx.graphics.getHeight();
+		gameWidth = 900;
+		gameHeight = 1600;
+		scaleRatio = deviceHeight/gameHeight;
+		camera = new OrthographicCamera(gameWidth,gameHeight);
+
+		arrayOfTiles = new Array<Float>();
 	}
 
 	@Override
 	public void render () {
 		Gdx.gl.glClearColor(1, 0, 0, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+
+		camera.update();
+		batch.setProjectionMatrix(camera.combined);
 		batch.begin();
-		batch.draw(img, 0, 0);
+		batch.draw(img,-img.getWidth()/2,-img.getHeight()/2);
 		batch.end();
+
 	}
 	
 	@Override
 	public void dispose () {
 		batch.dispose();
 		img.dispose();
+		imgTile.dispose();
 	}
 }
--- PianoTiles/desktop/src/com/donick/pianotiles/desktop/DesktopLauncher.java
+++ PianoTiles/desktop/src/com/donick/pianotiles/desktop/DesktopLauncher.java
@@ -7,6 +7,8 @@ import com.donick.pianotiles.PianoTiles;
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		config.width = 900/2;
+		config.height = 1600/2;
 		new LwjglApplication(new PianoTiles(), config);
 	}
 }

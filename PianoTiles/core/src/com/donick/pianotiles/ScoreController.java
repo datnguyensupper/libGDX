12f2e6b4fd3d84c155fdcbb0916cb05b97f10e89
Nguyen Tien Dat <tuynumemories2910@gmail.com>
2017-03-25 07:11:02 +0700

    add score controller

---
+++ PianoTiles/core/src/com/donick/pianotiles/ScoreController.java
@@ -0,0 +1,12 @@
+package com.donick.pianotiles;
+
+import com.badlogic.gdx.utils.Array;
+
+/**
+ * Created by nguyen on 3/22/2017.
+ */
+public class ScoreController {
+
+    private float currentScore;
+    private float maxScore;
+}
